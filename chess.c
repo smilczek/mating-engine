81da404b23a7ef65e81c9500c7121a585b4e900d
smilczek <szymon@milczek.com>
2026-07-25 20:03:40 +0200

    parseFEN(), returns BoardState

--- chess.c
+++ chess.c
@@ -6,10 +6,15 @@
 #define false 0
 #define true 1
 
+
 typedef struct {
-    int Turn;
-    bool BlackToPlay;
+    int Rank, File;
+} Coord;
+
+typedef struct {
     char Board[BOARDSIZE * BOARDSIZE];
+
+    bool BlackToMove;
 
     // Castling rights
     bool CR_WK; // White King
@@ -19,11 +24,15 @@ typedef struct {
 
     // En passant target
     char EnPassant[2]; // e.g. e3
-} BoardState;
 
-typedef struct {
-    int Rank, File;
-} Coord;
+    // 50-move rule
+    int HalfmoveClock;
+
+    // Starts at 0. (Normally starts at 1, stupid,
+    // I'd rather keep ZII, add 1 for display)
+    // Increments after black's move.
+    int FullmoveNumber;
+} BoardState;
 
 
 static char lowercase(char c) {
@@ -45,8 +54,8 @@ static void initBoardState(BoardState *BS) {
     const char *PawnSetup =     "PPPPPPPP";
     // TODO(smilczek) verify BackrankSetup is of BOARDSIZE length
 
-    BS->Turn = 0;
-    BS->BlackToPlay = 0;
+    BS->FullmoveNumber = 0;
+    BS->BlackToMove = 0;
 
     BS->CR_WK = true;
     BS->CR_WQ = true;
@@ -86,4 +95,102 @@ static Coord parseCoordinateStr(char *CoordStr) {
     Ret.Rank = CoordStr[0] - 'a';
     Ret.File = CoordStr[1] - '1';
     return Ret;
+}
+
+// assumes null-terminated string.
+// undef behavior for invalid FEN.
+// TODO(smilczek): FEN validation (separate func).
+static BoardState parseFEN(char *FENStr) {
+    BoardState BS = {0};
+
+    int Rank = 7; // 8
+    int File = 0; // A
+    while (*FENStr != ' ') {
+        char Piece = *FENStr++;
+        if (Piece >= '1' && Piece <= '8') {
+            // Denotes how many empty squares there are to skip.
+            File += Piece - '0';
+            continue;
+        }
+        if (lowercase(Piece) >= 'a' && lowercase(Piece) <= 'z') {
+            // Assume it's a valid piece (RNBKQ or rnbkq)
+            BS.Board[Rank * BOARDSIZE + File] = Piece;
+            File++;
+            continue;
+        }
+
+        // must be '/' character, denoting end of rank.
+        Rank--;
+        File = 0;
+    }
+
+    FENStr++;
+    // w or b (which to move)
+    switch (*FENStr) {
+        case 'b': {
+            BS.BlackToMove = true;
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+
+    FENStr++;
+    // castling rights
+    while (*(++FENStr) != ' ') {
+        switch (*FENStr) {
+            case 'K': {
+                BS.CR_WK = true;
+                break;
+            }
+            case 'Q': {
+                BS.CR_WQ = true;
+                break;
+            }
+            case 'k': {
+                BS.CR_BK = true;
+                break;
+            }
+            case 'q': {
+                BS.CR_BQ = true;
+                break;
+            }
+            default: {
+                // Should be '-'
+                break;
+            }
+        }
+    }
+
+    FENStr++;
+    // En Passant target
+    if (*FENStr != '-') {
+        BS.EnPassant[0] = *FENStr++;
+        BS.EnPassant[1] = *FENStr++;
+    } else {
+        FENStr++;
+    }
+
+    FENStr++;
+    // Halfmove clock (for 50-move rule)
+    BS.HalfmoveClock = 0;
+    while (*FENStr != ' ') {
+        BS.HalfmoveClock *= 10;
+        char Digit = *FENStr++;
+        BS.HalfmoveClock += Digit - '0';
+    }
+
+    FENStr++;
+    // FullmoveNumber
+    BS.FullmoveNumber = 0;
+    while (*FENStr) {
+        BS.FullmoveNumber *= 10;
+        char Digit = *FENStr++;
+        BS.FullmoveNumber += Digit - '0';
+    }
+    // FEN fullmove starts at 1. I want to keep ZII.
+    BS.FullmoveNumber -= 1;
+
+    return BS;
 }
--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include "chess.c"
+#include "string.h"
 
 static bool test_lowercase() {
     bool Success = true;
@@ -41,10 +42,76 @@ static bool test_parseCoordinateStr() {
     return Success;
 }
 
+static bool test_parseFEN() {
+    bool Success = true;
+
+    // Starting position.
+    BoardState BS = parseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    Success &= memcmp(BS.Board, "RNBQKBNR"
+                                "PPPPPPPP"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "pppppppp"
+                                "rnbqkbnr", 64) == 0;
+    Success &= BS.BlackToMove == false;
+    Success &= BS.CR_WK &&
+               BS.CR_WQ &&
+               BS.CR_BK &&
+               BS.CR_BQ;
+    Success &= BS.EnPassant[0] == '\0';
+    Success &= BS.EnPassant[1] == '\0';
+    Success &= BS.HalfmoveClock == 0;
+    Success &= BS.FullmoveNumber == 0;
+
+    // En passant
+    BS = parseFEN("4k3/8/8/8/3pP3/8/8/4K3 b - e3 0 1");
+    Success &= memcmp(BS.Board, "\0\0\0\0K\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0pP\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0k\0\0\0", 64) == 0;
+    Success &= BS.BlackToMove == true;
+    Success &= BS.EnPassant[0] == 'e';
+    Success &= BS.EnPassant[1] == '3';
+    Success &= !(BS.CR_WK ||
+                 BS.CR_WQ ||
+                 BS.CR_BK ||
+                 BS.CR_BQ);
+
+    // Fullmove, Halfmove
+    BS = parseFEN("r1bqkb1r/pppppppp/2n2n2/8/8/2N2N2/PPPPPPPP/R1BQKB1R w KQkq - 4 3");
+    Success &= memcmp(BS.Board, "R\0BQKB\0R"
+                                "PPPPPPPP"
+                                "\0\0N\0\0N\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0\0\0\0\0\0\0"
+                                "\0\0n\0\0n\0\0"
+                                "pppppppp"
+                                "r\0bqkb\0r", 64) == 0;
+    Success &= BS.HalfmoveClock == 4;
+    Success &= BS.FullmoveNumber == 2;
+
+    // Double digit parsing
+    BS = parseFEN("r1bqkb1r/pppppppp/2n2n2/8/8/2N2N2/PPPPPPPP/R1BQKB1R w KQkq - 40 25");
+    Success &= BS.HalfmoveClock == 40;
+    Success &= BS.FullmoveNumber == 24;
+
+    return Success;
+}
+
 int main() {
     bool Success = true;
-    Success &= test_parseCoordinateStr();
     Success &= test_lowercase();
+    assert(Success);
+    Success &= test_parseCoordinateStr();
+    assert(Success);
+    Success &= test_parseFEN();
+    assert(Success);
 
     return Success;
 }
