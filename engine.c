fb2b1fb93f8ecb02b08e4dbd27bd78c3da966741
smilczek <szymon@milczek.com>
2026-08-01 12:44:30 +0200

    solves 2 move puzzles

--- engine.c
+++ engine.c
@@ -1,3 +1,104 @@
-Move eng_findBestMove(BoardState *BS) {
-    MoveList LegalMoves = ch_getLegalMoves(BS);
+#ifndef _ENGINE_C_
+#define _ENGINE_C_
+
+// Half moves.
+#define DEPTH 4
+
+#define INFINITY (__builtin_inff())
+
+typedef struct {
+    Move Moves[DEPTH]; // White and Black
+    float Eval;
+} Sequence;
+
+typedef struct {
+    Sequence List[1];
+} SequenceList;
+
+float en_evaluatePosition(BoardState *BS) {
+    // TODO(smilczek): Test if counting the legal moves for
+    //                 each side is also a valid eval strat.
+    //                 (The diff MyMoves - TheirMoves)
+    float PAWN   = 1.0f;
+    float KNIGHT = 3.0f;
+    float BISHOP = 3.0f;
+    float ROOK   = 5.0f;
+    float QUEEN  = 9.0f;
+    float Eval = 0.0f;
+    for (int R = 0; R < BOARDSIZE; ++R) {
+        for (int F = 0; F < BOARDSIZE; ++F) {
+            char P = ch_pieceAt(BS, R, F);
+            float BlackMul = ch_isBlackPiece(P) ? -1.0f : 1.0f;
+            float PieceVal = 0.0f;
+            switch (lowercase(P)) {
+                case 'p': {
+                    PieceVal = PAWN;
+                    break;
+                }
+                case 'n': {
+                    PieceVal = KNIGHT;
+                    break;
+                }
+                case 'b': {
+                    PieceVal = BISHOP;
+                    break;
+                }
+                case 'r': {
+                    PieceVal = ROOK;
+                    break;
+                }
+                case 'q': {
+                    PieceVal = QUEEN;
+                    break;
+                }
+                default: {
+                    // always two kings, no point.
+                    break;
+                }
+            }
+            PieceVal *= BlackMul;
+            Eval += PieceVal;
+        }
+    }
+    // BoardState BSCopy = *BS;
+    // BSCopy.BlackToMove = false;
+    // MoveList WhiteMoves = ch_generatePseudoLegalMoves(&BSCopy);
+    // BSCopy.BlackToMove = true;
+    // MoveList BlackMoves = ch_generatePseudoLegalMoves(&BSCopy);
+    // return (float)(WhiteMoves.Count - BlackMoves.Count);
+    return Eval;
 }
+
+Sequence en_recurrentEvaluateMove(BoardState *BS, Sequence Seq, int Depth) {
+    if (Depth == DEPTH) {
+        Seq.Eval = en_evaluatePosition(BS);
+        return Seq;
+    }
+    bool BlackToMove = BS->BlackToMove;
+
+    Sequence CurrBestSeq = Seq;
+    CurrBestSeq.Eval = BlackToMove ? INFINITY : -INFINITY;
+    MoveList LegalMoves = ch_getLegalMoves(BS);
+    for (int I = 0; I < LegalMoves.Count; ++I) {
+        Move Mv = LegalMoves.List[I];
+
+        BoardState BSCopy = *BS;
+        ch_applyMove(&BSCopy, Mv);
+        Seq.Moves[Depth] = Mv;
+        Sequence CurrSeq = en_recurrentEvaluateMove(&BSCopy, Seq, Depth + 1);
+
+        if (BlackToMove && CurrSeq.Eval < CurrBestSeq.Eval ||
+                !BlackToMove && CurrSeq.Eval > CurrBestSeq.Eval) {
+            CurrBestSeq = CurrSeq;
+        }
+    }
+    return CurrBestSeq;
+}
+
+SequenceList en_findBestSequence(BoardState *BS) {
+    SequenceList SeqList = {0};
+    SeqList.List[0] = en_recurrentEvaluateMove(BS, (Sequence){0}, 0);
+    return SeqList;
+}
+
+#endif
--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include "base.h"
 #include "chess.h"
 #include "chess.c"
+#include "engine.c"
 #include "string.h"
 
 #include <stdio.h>
@@ -326,6 +327,27 @@ static bool test_filterLegalMoves() {
     return Success;
 }
 
+static bool test_findBestSequence() {
+    bool Success = true;
+    // The engine must make the obvious best moves.
+    // Take the free piece
+    BoardState BS = ch_parseFEN("r1bqkbnr/pppppppp/8/8/3n4/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 1");
+    SequenceList SeqL = en_findBestSequence(&BS);
+    Success &= SeqL.List[0].Moves[0].From.Rank == 2;
+    Success &= SeqL.List[0].Moves[0].From.File == 5;
+    Success &= SeqL.List[0].Moves[0].To.Rank == 3;
+    Success &= SeqL.List[0].Moves[0].To.File == 3;
+
+    BS = ch_parseFEN("rnb1kb1r/pp3ppp/8/q2np3/8/2NBBP2/PP4PP/R2QK1NR w KQkq - 0 11");
+    SeqL = en_findBestSequence(&BS);
+    Success &= SeqL.List[0].Moves[0].From.Rank == 2;
+    Success &= SeqL.List[0].Moves[0].From.File == 3;
+    Success &= SeqL.List[0].Moves[0].To.Rank == 4;
+    Success &= SeqL.List[0].Moves[0].To.File == 1;
+
+    return Success;
+}
+
 int main() {
     bool Success = true;
     Success &= test_lowercase();
@@ -345,6 +367,8 @@ int main() {
     Success &= test_filterLegalMoves();
     assert(Success);
     Success &= test_applyMove();
+    assert(Success);
+    Success &= test_findBestSequence();
     assert(Success);
 
 
