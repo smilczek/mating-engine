643b79c76733954cea121b2b2c175ca01497c94b
smilczek <szymon@milczek.com>
2026-08-02 01:37:18 +0200

    minor change to main

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 
 static const char *STARTING_POSITION_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-static BoardState g_BoardState = {0};
-
 static void printBoardState(BoardState *BS) {
     for (int Rank = BOARDSIZE - 1; Rank > -1; --Rank) {
         for (int File = 0; File < BOARDSIZE; ++File) {
@@ -32,7 +30,8 @@ static void printMove(BoardState *BS, Move Mv) {
 }
 
 int main() {
-    BoardState BS = ch_parseFEN("6R1/4r2p/5ppk/1b6/1B3p2/1BP5/PP4PP/6K1 b - - 3 34");
+    // BoardState BS = ch_parseFEN("6R1/4r2p/5ppk/1b6/1B3p2/1BP5/PP4PP/6K1 b - - 3 34");
+    BoardState BS = ch_parseFEN(STARTING_POSITION_FEN);
     SequenceList SeqL = en_findBestSequence(&BS);
     for (int i = 0; i < arr_len(SeqL.List[0].Moves); i += 2) {
         printf("%d. ", (i + 2 / 2));
@@ -41,5 +40,6 @@ int main() {
         printMove(&BS, SeqL.List[0].Moves[i + 1]);
         printf(" ");
     }
+    printf("\n");
     return 0;
 }
